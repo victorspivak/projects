ebbd2b772e149fe7e29523df97c70c9477402f35
Victor Spivak <victor.spivak@gmail.com>
2012-09-16 10:53:41 -0700

    Modified Enum.scala

--- Learn/LearnScala/src/svl/learn/scala/basic/Enum.scala
+++ Learn/LearnScala/src/svl/learn/scala/basic/Enum.scala
@@ -9,3 +9,37 @@ import WeekDay._
 def isWorkingDay(d: WeekDay) = ! (d == Sat || d == Sun)
 
 WeekDay.values filter isWorkingDay foreach println
+
+object Colors extends Enumeration {
+    type Colors = Value
+    val Black, White, Red, Green, Yellow, Blue = Value
+}
+
+object TrafficColors extends Enumeration {
+    type TrafficColors = Value
+    val Red, Green, Yellow = Value
+}
+
+import TrafficColors._
+
+implicit def TrafficColor2Color(color: TrafficColors) = Colors.withName(color.toString)
+
+object ColorProcessor {
+    import Colors._
+
+    def fun1(color:Colors) {
+        println(color)
+    }
+}
+
+def fun2(color:TrafficColors) {
+    ColorProcessor.fun1(color)
+}
+
+fun2(Red)
+
+type MyColors = TrafficColors
+
+val aaa:MyColors = Green
+
+println(aaa)
