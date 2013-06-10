0ff400f9fddf6293b021531808dc1d0b2e820539
Victor Spivak <victor.spivak@gmail.com>
2013-06-09 17:38:05 -0700

    Added a few examples for AnyRef and Any

--- Learn/LearnScala/src/svl/learn/scala/basic/BasicClasses.scala
+++ Learn/LearnScala/src/svl/learn/scala/basic/BasicClasses.scala
@@ -1,9 +1,12 @@
-object BasicClasses {
-    def main(args:Array[String]) {
-        val hello : Any = "Hello"
-        Console println  hello.hashCode
-        Console println  hello.##
-        Console println  (hello ##)
-    }
+object BasicClasses extends App {
+    val hello : Any = "Hello"
+    Console println  hello.hashCode
+    Console println  hello.##
+    Console println  (hello ##)
+
+    //val anyRef1:AnyRef = 5  //is compile error
+    val anyVal1:AnyVal = 5
+    //val anyVal2:AnyVal = "" //is compile error
+
 }
 
