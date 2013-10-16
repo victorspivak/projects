0f18e66f623c02d5dd526d89f6344504855790aa
Victor Spivak <victor.spivak@gmail.com>
2013-10-16 10:35:28 -0700

    Updated Future Try sample

--- Learn/LearnScala/src/svl/learn/scala/monads/FutureAndTryModified.scala
+++ Learn/LearnScala/src/svl/learn/scala/monads/FutureAndTryModified.scala
@@ -12,33 +12,90 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * User: victor    Date: 10/12/13   Time: 5:46 PM
  */
 object FutureAndTryModified {
-    def func(value:Int): Int = {
-        if (value == 0) {
-            println("************************************")
-            throw new Exception("oops")
-        }
-        value
-    }
+	def func(value: Int): Int = {
+		Thread.sleep(1000)
+		if (value == 0) {
+			println("************************************")
+			throw new Exception("oops")
+		}
+		Thread.sleep(1000)
+		value
+	}
 
-    def main(args: Array[String]) {
-        test1()
-    }
+	def main(args: Array[String]) {
+		test1()
+//		test2()
+	}
 
-    def test1 (){
-        val f1 = future{Try(func(0))}
-        val f1_1 = future{Try(func(1))}
-        val f2 = f1.map{_.map{
-            _+10
-        }}
+	def test1() {
+		implicit def tryToFuture[T](t: Try[T]): Future[T] = {
+			t match {
+				case Success(s) => Future.successful(s)
+				case Failure(ex) => Future.failed(ex)
+			}
+		}
 
-        val f3 = f2.map{
-            _.flatMap{v =>
-                Try(v + func(100))}
-        }
-        val f4 = f1_1.map{_.map(_+1000)}
+		val f1 = future {
+			Try{
+				println("===> f1 started")
+				func(0)
+			}
+		}
+		val f1_1 = future {
+			Try{
+				println("===> f2 started")
+				func(1)
+			}
+		}
+		val f2 = f1.flatMap {
+			_.map {
+				_ + 10
+			}
+		}
 
-        val result = Await result (Future.sequence(List(f3, f4)), 10.second)
+		val f3 = f2.flatMap {v =>
+			Try(v + func(100))
+		}
+		val f4 = f1_1.flatMap {
+			_.map(_ + 1000)
+		}
 
-        println(result)
-    }
+		val f: Future[List[Int]] = Future.sequence(List(f3, f4))
+
+		println("Before...")
+		val result = Await result(f, 10.second)
+		println("After...")
+
+		println(result)
+	}
+
+	def test2() {
+		val f1 = future {
+			Try(func(0))
+		}
+		val f1_1 = future {
+			Try(func(1))
+		}
+		val f2 = f1.map {
+			_.map {
+				_ + 10
+			}
+		}
+
+		val f3 = f2.map {
+			_.flatMap {
+				v =>
+					Try(v + func(100))
+			}
+		}
+		val f4 = f1_1.map {
+			_.map(_ + 1000)
+		}
+
+		val f: Future[List[Try[Int]]] = Future.sequence(List(f3, f4))
+
+		val result = Await result(f, 10.second)
+
+		println(result)
+	}
 }
