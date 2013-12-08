15f9ab9d073d655e5970dafdb17a04f9de35f890
Victor Spivak <victor.spivak@gmail.com>
2013-12-08 01:24:48 -0800

    Added SBT Template for Scala

---
+++ zSbtScalaProjectTemplate/project/build.scala
@@ -0,0 +1,56 @@
+import sbt._
+import sbt.Keys._
+
+object MyScalaLibBuild extends Build {
+  import com.github.retronym.SbtOneJar._
+  import sbtbuildinfo.Plugin._
+
+  val MyOrganization = "svl"
+  val MyArtifact = "MyScalaLib"
+  val MyPackage = "svl.???"
+  val MyProjectVersion = "1.0.0"
+
+  val ScalaVersion = "2.10.3"
+  val ScalaVersionDep = "2.10"
+  val ScalatraVersion = "2.2.1"
+  val Slf4jVersion = "1.7.5"
+  val JUnitVersion = "4.7"
+  val MochitoVersion = "1.9.0"
+  val Specs2Version = "2.0"
+
+  lazy val deps = Seq(
+    "org.slf4j" % "slf4j-simple" % Slf4jVersion,
+    "commons-logging" % "commons-logging" % "1.1.1",
+    "org.specs2" % ("specs2_" + ScalaVersionDep) % Specs2Version % "test",
+    "org.mockito"              % "mockito-all"          % MochitoVersion  % "test",
+    "junit"                    % "junit"                % JUnitVersion    % "test"
+  )
+
+  val root = Project(
+    id = "myscalalib",
+    base = file("."),
+    settings =
+      Defaults.defaultSettings ++
+      oneJarSettings ++
+      buildInfoSettings ++
+      Seq(
+        organization := MyOrganization,
+        version := MyProjectVersion,
+        scalaVersion := ScalaVersion,
+        libraryDependencies ++= deps,
+        sourceGenerators in Compile <+= buildInfo,
+        buildInfoPackage := MyPackage,
+        connectInput in run := true,
+        publishArtifact in (Compile, packageDoc) := false,
+        publishArtifact in (Compile, packageSrc) := false,
+        publishArtifact in Test := false,
+
+        fork in run := true,
+        javaOptions ++= Seq("-deprecation", "-feature", "-unchecked"),
+        initialCommands := """
+                             |import svl.???._
+                             |""".stripMargin
+      ) ++
+      addArtifact(Artifact(MyArtifact, "one-jar"), oneJar)
+  )
+}
