4e96ea7d02c5904427b0daddcf73c02bce7799ca
vspivak <victor.spivak@gmail.com>
2014-07-20 15:33:50 -0700

    LearnScala: Updated combinators

--- Learn/LearnScala/src/main/scala/svl/learn/scala/combinator/CalculatorWithVariables.scala
+++ Learn/LearnScala/src/main/scala/svl/learn/scala/combinator/CalculatorWithVariables.scala
@@ -1,182 +1,79 @@
 package svl.learn.scala.combinator
 
-import scala.collection.mutable
+import scala.util.Try
 import scala.util.parsing.combinator.RegexParsers
 
 object CalculatorWithVariables extends App {
   import svl.learn.scala.combinator.CalculatorWithVariables.Calculator.Vars
 
-  import scala.collection.mutable.Map
+  import scala.collection.mutable
 
   class Calculator(val variables:Vars) extends RegexParsers {
+    def identificatorRegex = """[a-zA-Z]([a-zA-Z0-9])*""".r
 
-    //  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-    //  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
-    //  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
-    //    case number ~ list => (number /: list) {
-    //      case (x, "*" ~ y) => x * y
-    //      case (x, "/" ~ y) => x / y
-    //    }
-    //  }
-
-    def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ {
-      _.toDouble
+    def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ {_.toDouble}
+    def variable: Parser[Double] = identificatorRegex ^^ {id: String =>
+      variables.get(id) match {
+        case Some(value) => value
+        case None =>
+          throw new IllegalArgumentException(s"Error. Could not find variable $id")
+      }
+    }
+    def numberOrVariable: Parser[Double] = number | variable
+    def factor: Parser[Double] = numberOrVariable | "(" ~> expr <~ ")"
+    def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
+      case numberOrVariable ~ list => (numberOrVariable /: list) {
+        case (x, "*" ~ y) => x * y
+        case (x, "/" ~ y) => x / y
+      }
     }
 
-    def variable: Parser[String] = """[a-zA-Z]([a-zA-Z0-9])*""".r ^^ { id: String => id}
-
-    def assignment: Parser[String] = variable ~ "=" ^^ {
-      _._1
+//For enable logging use the following line
+//    def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
+    def expr  : Parser[Double] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
+      case numberOrVariable ~ list => list.foldLeft(numberOrVariable) { // same as before, using alternate name for /:
+        case (x, "+" ~ y) => x + y
+        case (x, "-" ~ y) => x - y
+      }
     }
 
-    def statement: Parser[Double] = assignment ~ number ~ ";" ^^ {
+    def assignment: Parser[String] = identificatorRegex ~ "=" ^^ {case (name ~ "=") => name}
+
+    def statement: Parser[Unit] = assignment ~ expr ~ ";" ^^ {
       case name ~ value ~ semicolon =>
-
-        println(name + " -> " + value)
-        value
+        variables.put(name, value)
     }
 
-    def statements: Parser[Double] = statement ~ rep(statement) ^^ {
-      case name ~ list => 5.0
+    def statements: Parser[Vars] = statement ~ rep(statement) ^^ {
+      case name ~ list => variables
     }
 
-    ////  def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
-    //  def expr  : Parser[Double] = variable ~ "=" ~ number ^^ {
-    //    case number ~ list => list.foldLeft(number) { // same as before, using alternate name for /:
-    //      case (x, "+" ~ y) => x + y
-    //      case (x, "-" ~ y) => x - y
-    //    }
-    //  }
-
-    def calculate(input: String): Double = parseAll(statements, input) match {
-      case Success(result, _) => result
-      case failure: NoSuccess => scala.sys.error(failure.msg)
+    def calculate(input: String): Try[Vars] =
+      Try{
+        parseAll(statements, input) match {
+          case Success(result, _) => variables
+          case failure: NoSuccess => scala.sys.error(failure.msg)
+        }
+      }
     }
-  }
 
   object Calculator {
     type Vars = mutable.Map[String, Double]
-    def apply(input: String): Double = new Calculator(mutable.Map()).calculate(input)
+    def apply(input: String): Try[Vars] = new Calculator(mutable.Map()).calculate(input)
+    def apply(input: String, vars:Vars): Try[Vars] = new Calculator(vars).calculate(input)
   }
 
   val input =
     """
-      |abc = 5;
+      |abc = 5*5 + 3*4;
       |a = 10;
+      |b = abc*a + abc + a;
+      |c = z;
     """.stripMargin
-  println(Calculator(input))
+
+  val res1 = Calculator(input)
+  println(res1)
+
+  val res2 = Calculator(input, mutable.Map("z" -> 999))
+  println(res2)
 }
-
-//  type Vars = Map[String, Double]
-//
-//  //  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-//  //  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
-//  //  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
-//  //    case number ~ list => (number /: list) {
-//  //      case (x, "*" ~ y) => x * y
-//  //      case (x, "/" ~ y) => x / y
-//  //    }
-//  //  }
-//
-//  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-//  def variable: Parser[String] = """[a-zA-Z]([a-zA-Z0-9])*""".r ^^ {id:String => id}
-//  def assignment: Parser[String] = variable ~ "=" ^^ {_._1}
-//  def statement  : Parser[Double] = assignment ~ number ~ ";" ^^ {
-//    case name ~ value ~ semicolon =>
-//
-//      println(name)
-//      value
-//  }
-//
-//  def statements : Parser[Double] = statement ~ rep(statement)  ^^ {
-//    case name ~ list => 5.0
-//  }
-//
-//  ////  def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
-//  //  def expr  : Parser[Double] = variable ~ "=" ~ number ^^ {
-//  //    case number ~ list => list.foldLeft(number) { // same as before, using alternate name for /:
-//  //      case (x, "+" ~ y) => x + y
-//  //      case (x, "-" ~ y) => x - y
-//  //    }
-//  //  }
-//
-//  def apply(input: String): Double = parseAll(statements, input) match {
-//    case Success(result, _) => result
-//    case failure : NoSuccess => scala.sys.error(failure.msg)
-//  }
-//
-//  val input =
-//    """
-//      |abc = 5;
-//      |a = 10;
-//    """.stripMargin
-//  println(apply(input))
-//}
-
-//object CalculatorWithVariables extends RegexParsers with App{
-//  type Vars = Map[String, Double]
-//
-////  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-////  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
-////  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
-////    case number ~ list => (number /: list) {
-////      case (x, "*" ~ y) => x * y
-////      case (x, "/" ~ y) => x / y
-////    }
-////  }
-//
-//  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-//  def variable: Parser[String] = """[a-zA-Z]([a-zA-Z0-9])*""".r ^^ {id:String => id}
-//  def assignment: Parser[String] = variable ~ "=" ^^ {_._1}
-//  def statement  : Parser[Double] = assignment ~ number ~ ";" ^^ {
-//    case name ~ value ~ semicolon =>
-//
-//      println(name)
-//      value
-//  }
-//
-//  def statements : Parser[Double] = statement ~ rep(statement)  ^^ {
-//    case name ~ list => 5.0
-//  }
-//
-//////  def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
-////  def expr  : Parser[Double] = variable ~ "=" ~ number ^^ {
-////    case number ~ list => list.foldLeft(number) { // same as before, using alternate name for /:
-////      case (x, "+" ~ y) => x + y
-////      case (x, "-" ~ y) => x - y
-////    }
-////  }
-//
-//  def apply(input: String): Double = parseAll(statements, input) match {
-//    case Success(result, _) => result
-//    case failure : NoSuccess => scala.sys.error(failure.msg)
-//  }
-//
-//  val input =
-//    """
-//      |abc = 5;
-//      |a = 10;
-//    """.stripMargin
-//  println(apply(input))
-//}
-//
-//  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
-//  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
-//  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
-//    case number ~ list => (number /: list) {
-//      case (x, "*" ~ y) => x * y
-//      case (x, "/" ~ y) => x / y
-//    }
-//  }
-//  def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
-//    case number ~ list => list.foldLeft(number) { // same as before, using alternate name for /:
-//      case (x, "+" ~ y) => x + y
-//      case (x, "-" ~ y) => x - y
-//    }
-//  }
-//
-//  def apply(input: String): Double = parseAll(expr, input) match {
-//    case Success(result, _) => result
-//    case failure : NoSuccess => scala.sys.error(failure.msg)
-//  }
-
